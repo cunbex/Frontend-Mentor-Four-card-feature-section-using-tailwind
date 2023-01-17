d84e754f4b71d0ca03cff4fa5f046582185f3836
cunbex <hamzalagab.tech@gmail.com>
2023-01-17 12:44:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+      },
+      colors: {
+        "Very-Dark-Blue": "hsl(234, 12%, 34%)",
+        "Grayish-Blue": "hsl(229, 6%, 66%)",
+        "Very-Light-Gray": "hsl(0, 0%, 98%)",
+        "chal-Red": "hsl(0, 78%, 62%)",
+        "chal-Cyan": "hsl(180, 62%, 55%)",
+        "chal-Orange": "hsl(34, 97%, 64%)",
+        "chal-Blue": "hsl(212, 86%, 64%)",
+      },
+    },
+  },
+  plugins: [],
+};
